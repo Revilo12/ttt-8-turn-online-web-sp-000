4129e9a2b60a37e5d331af71f852c8bdfc83b5c7
Oliver Dahl <oliverpdahl@gmail.com>
2019-12-26 16:24:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,7 +2,22 @@ def turn(board)
   puts "Please enter 1-9:"
   index = gets.strip - 1
   if valid_move?(board, index)
-    display_board
+    display_board(move(board,index))
+  else
+    puts "Please enter a valid input"
+    turn(board)
+end
+
+def display_board(boardIn)
+  if boardIn == nil
+    puts "That was an unacceptable input"
+  else
+    puts " #{boardIn[0]} | #{boardIn[1]} | #{boardIn[2]} "
+    puts "-----------"
+    puts " #{boardIn[3]} | #{boardIn[4]} | #{boardIn[5]} "
+    puts "-----------"
+    puts " #{boardIn[6]} | #{boardIn[7]} | #{boardIn[8]} "
+  end
 end
 
 def move(boardIn, input_index, character = "X")
